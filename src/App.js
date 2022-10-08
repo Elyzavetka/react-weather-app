4017c39f875316e7bf229ba54b57235b10a6f27a
Elyzavetka <lizamybella@gmail.com>
2022-10-08 23:06:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by Liza Tarasova
-        <a href="https://polite-sherbet-e0fba5.netlify.app">Open-source code</a>
-        ;
-      </footer>
+      Weather app This project was coded by Liza Tarasova
+      <a href="https://polite-sherbet-e0fba5.netlify.app">Open-source code</a>;
     </div>
   );
 }
