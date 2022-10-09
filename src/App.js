7e99a896f0b5161d4fe963983c5b03d2b00145d1
Elyzavetka <lizamybella@gmail.com>
2022-10-09 21:25:15 +0100

    Added HTML structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div class="container">
         Weather app
-        <p>This project was coded by Liza Tarasova</p>
-        <a href="https://polite-sherbet-e0fba5.netlify.app">Open-source code</a>
-        ;
+        <Weather />
+        <p>
+          This project was coded by Liza Tarasova
+          <a href="https://polite-sherbet-e0fba5.netlify.app">
+            Open-source code
+          </a>
+          ;
+        </p>
       </div>
     </div>
   );
