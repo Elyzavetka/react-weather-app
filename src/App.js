b60d5e56a5f2143ad238f6681c4cf8d406387108
Elyzavetka <lizamybella@gmail.com>
2022-10-09 17:10:59 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <p>This project was coded by Liza Tarasova</p>
-      <a href="https://polite-sherbet-e0fba5.netlify.app">Open-source code</a>;
+      <div class="container">
+        Weather app
+        <p>This project was coded by Liza Tarasova</p>
+        <a href="https://polite-sherbet-e0fba5.netlify.app">Open-source code</a>
+        ;
+      </div>
     </div>
   );
 }
